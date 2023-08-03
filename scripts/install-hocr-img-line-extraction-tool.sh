12f1f16f6df99e89f40b906e4c2777d0b5ed37d1
Daniel_Stoxreiter <daniel.stoxreiter@outlook.de>
2023-08-03 14:26:29 +0200

    change hocr to release zip

--- scripts/install-hocr-img-line-extraction-tool.sh
+++ scripts/install-hocr-img-line-extraction-tool.sh
@@ -1,6 +1,7 @@
 #/bish/bash
 
 mkdir -p $DATA_DIR_TRAINING/$MODEL_NAME-ground-truth
-git clone git@github.com:linxOD/hocr-img-line-extraction.git
-cd hocr-img-line-extraction
+wget https://github.com/linxOD/hocr-img-line-extraction/archive/refs/tags/v0.0.1.zip
+unzip hocr-img-line-extraction-0.0.1.zip
+cd hocr-img-line-extraction-0.0.1
 ./scripts/install_dep.sh
